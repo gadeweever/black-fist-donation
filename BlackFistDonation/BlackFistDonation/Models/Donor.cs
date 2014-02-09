01a4fcc5c5c19054423b1ca3b82382978d768067
gadeweever <gamaldeweever@gmail.com>
2014-02-09 01:15:22 -0600

    context creation

---
+++ BlackFistDonation/BlackFistDonation/Models/Donor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlackFistDonation.Models
+{
+    public class Donor
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Donation { get; set; }
+    }
+
+    public class DonorDBContext : DbContext
+    {
+        public DbSet<Donor> Donors { get; set; }
+    }
+}
---
+++ BlackFistDonation/BlackFistDonation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlackFistDonation.Startup))]
+namespace BlackFistDonation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
